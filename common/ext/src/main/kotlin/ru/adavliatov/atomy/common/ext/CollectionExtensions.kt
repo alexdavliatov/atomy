0d29e7703ea5630315e1ab1e0ad1162d13c83e3f
adavliatov <adavliatov@yandex.ru>
2019-12-02 23:16:45 +0300

    CollectionExtensions.kt are added.

---
+++ common/ext/src/main/kotlin/ru/adavliatov/atomy/common/ext/CollectionExtensions.kt
@@ -0,0 +1,18 @@
+package ru.adavliatov.atomy.common.ext
+
+import java.util.*
+
+object CollectionExtensions {
+  fun <T, R> Iterable<T>.mapToSet(transform: (T) -> R): Set<R> = mapTo(HashSet(), transform)
+  fun <T, R> Array<T>.mapToSet(transform: (T) -> R): Set<R> = mapTo(HashSet(), transform)
+  fun <T, R> Iterable<T>.flatMapToSet(transform: (T) -> Iterable<R>): Set<R> = flatMapTo(HashSet(), transform)
+  fun <T, R> Sequence<T>.mapToSet(transform: (T) -> R): Set<R> = mapTo(HashSet(), transform)
+  fun <T, R : Any> Iterable<T>.mapNotNullToSet(transform: (T) -> R): Set<R> = mapNotNullTo(HashSet(), transform)
+  fun <T> Iterable<T>?.toSetOrEmpty(): Set<T> = this?.toSet() ?: setOf()
+  fun <T> Iterable<T>?.toListOrEmpty(): List<T> = this?.toList() ?: listOf()
+  fun <T> List<T>.getOrThrow(index: Int, exception: () -> RuntimeException): T =
+    getOrNull(index) ?: throw exception()
+
+  fun <T> Iterable<T>.firstOrThrow(error: () -> RuntimeException): T = firstOrNull() ?: throw error()
+  fun <T> Sequence<T>.firstOrThrow(error: () -> RuntimeException): T = firstOrNull() ?: throw error()
+}
