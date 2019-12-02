66c81f727326cd88055e26e15726b13ef939ae97
adavliatov <adavliatov@yandex.ru>
2019-12-02 22:54:05 +0300

    Transfer example modules are introduced.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,24 @@
 rootProject.name = "ru.adavliatov.atomy"
 
-include()
+include(
+    ":common",
+    ":common:ext",
+    ":common:type:json",
+    ":common:domain",
+    ":common:service",
+    ":common:ui:api",
+    ":common:ui:cmd",
+    ":common:app",
+
+    ":toolkit",
+    ":toolkit:jooq",
+
+    ":example",
+    ":example:transfer",
+    ":example:transfer:ext",
+    ":example:transfer:domain",
+    ":example:transfer:service",
+    ":example:transfer:ui:api",
+    ":example:transfer:ui:cmd",
+    ":example:transfer:app"
+)
