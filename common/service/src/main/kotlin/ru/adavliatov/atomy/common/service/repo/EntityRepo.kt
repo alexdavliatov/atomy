56327bda0cf2ef88213b75811ca3620e91d26ff9
adavliatov <adavliatov@yandex.ru>
2019-12-03 10:38:27 +0300

    insertOrIgnore is renamed to fetchOrCreate. insert -> create. update -> modify. delete -> remove.

--- common/service/src/main/kotlin/ru/adavliatov/atomy/common/service/repo/EntityRepo.kt
+++ common/service/src/main/kotlin/ru/adavliatov/atomy/common/service/repo/EntityRepo.kt
@@ -5,22 +5,19 @@ import ru.adavliatov.atomy.common.domain.States.DELETED
 import ru.adavliatov.atomy.common.domain.WithEntity
 import ru.adavliatov.atomy.common.domain.WithModel
 import ru.adavliatov.atomy.common.domain.ext.ModelExtensions.ids
-import ru.adavliatov.atomy.common.service.domain.error.RepoErrors.NotFoundRepoError
 
 interface EntityRepo<Entity : WithEntity<Entity>> : ReadOnlyEntityRepo<Entity>, WriteOnlyEntityRepo<Entity>
 
 @Suppress("unused")
 interface ModelRepo<Model : WithModel<Model>> : EntityRepo<Model> {
-  override fun delete(model: Model) {
+  override fun remove(model: Model) {
     @Suppress("UNCHECKED_CAST")
-    update(model.withState(DELETED) as Model)
+    modify(model.withState(DELETED) as Model)
   }
 }
 
 @Suppress("unused")
-interface ReadOnlyEntityRepo<Entity : WithEntity<Entity>> {
-  fun findById(id: Id<Entity>): Entity?
-  fun findByIdChecked(id: Id<Entity>) = findById(id) ?: throw NotFoundRepoError(id)
+interface ReadOnlyEntityRepo<Entity : WithEntity<Entity>> : WithFindByIds<Entity> {
   fun findAll(): Iterable<Entity>
 
   fun count(): Int = findAll().count()
@@ -31,18 +28,18 @@ interface ReadOnlyEntityRepo<Entity : WithEntity<Entity>> {
 
 @Suppress("unused")
 interface WriteOnlyEntityRepo<Entity : WithEntity<Entity>> {
-  fun insert(model: Entity)
-  fun insert(vararg models: Entity)
-  fun insert(models: Collection<Entity>)
+  fun create(model: Entity)
+  fun create(vararg models: Entity)
+  fun create(models: Collection<Entity>)
 
-  fun update(model: Entity): Entity
-  fun update(vararg models: Entity)
-  fun update(models: Collection<Entity>)
+  fun modify(model: Entity): Entity
+  fun modify(vararg models: Entity)
+  fun modify(models: Collection<Entity>)
 
-  fun deleteByIds(vararg ids: Id<Entity>)
-  fun deleteByIds(ids: Collection<Id<Entity>>)
+  fun removeByIds(vararg ids: Id<Entity>)
+  fun removeByIds(ids: Collection<Id<Entity>>)
 
-  fun delete(model: Entity) = deleteByIds(model.id)
-  fun delete(vararg models: Entity) = deleteByIds(models.toList().ids())
-  fun delete(models: Collection<Entity>) = deleteByIds(models.ids())
+  fun remove(model: Entity) = removeByIds(model.id)
+  fun remove(vararg models: Entity) = removeByIds(models.toList().ids())
+  fun remove(models: Collection<Entity>) = removeByIds(models.ids())
 }
--- common/service/src/main/kotlin/ru/adavliatov/atomy/common/service/repo/behaviours.kt
+++ common/service/src/main/kotlin/ru/adavliatov/atomy/common/service/repo/behaviours.kt
@@ -1,18 +1,23 @@
 package ru.adavliatov.atomy.common.service.repo
 
-import ru.adavliatov.atomy.common.domain.*
+import ru.adavliatov.atomy.common.domain.Id
+import ru.adavliatov.atomy.common.domain.Refs
+import ru.adavliatov.atomy.common.domain.WithEntity
+import ru.adavliatov.atomy.common.domain.WithRef
 import ru.adavliatov.atomy.common.ext.CollectionExtensions.mapToSet
+import ru.adavliatov.atomy.common.service.domain.error.RepoErrors.NotFoundRepoError
 
 @Suppress("unused")
-interface WithInsertOrIgnore<One> {
-  fun insertOrIgnore(model: One): One
-  fun insertOrIgnore(models: Iterable<One>): Set<One> = models
-    .mapToSet { insertOrIgnore(it) }
+interface WithFetchOrCreate<One> {
+  fun fetchOrCreate(model: One): One
+  fun fetchOrCreate(models: Iterable<One>): Set<One> = models.mapToSet { fetchOrCreate(it) }
 }
 
 @Suppress("unused")
 interface WithFindByIds<Entity : WithEntity<Entity>> {
-  fun findByIds(ids: Set<Id<Entity>>): Set<Entity>
+  fun findById(id: Id<Entity>): Entity? = findByIds(setOf(id)).firstOrNull()
+  fun findByIdChecked(id: Id<Entity>) = findById(id) ?: throw NotFoundRepoError(id)
+  fun findByIds(ids: Iterable<Id<Entity>>): Set<Entity>
 }
 
 @Suppress("unused")
