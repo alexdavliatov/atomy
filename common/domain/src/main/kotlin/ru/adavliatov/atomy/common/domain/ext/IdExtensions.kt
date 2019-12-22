fc88e4de0454ef068b4607499819d2995f54a1db
Aleksandr Davliatov <alexander.davliatov@gmail.com>
2019-12-22 10:43:59 +0300

    Ids `checkedIdsToArray` extensions is added.

--- common/domain/src/main/kotlin/ru/adavliatov/atomy/common/domain/ext/IdExtensions.kt
+++ common/domain/src/main/kotlin/ru/adavliatov/atomy/common/domain/ext/IdExtensions.kt
@@ -1,10 +1,9 @@
 package ru.adavliatov.atomy.common.domain.ext
 
-import ru.adavliatov.atomy.common.domain.ConsumerRef
-import ru.adavliatov.atomy.common.domain.Id
-import ru.adavliatov.atomy.common.domain.WithEntity
+import ru.adavliatov.atomy.common.domain.*
 
 object IdExtensions {
   fun <E : WithEntity<E>> Iterable<Id<E>>.checkedIds(): Iterable<Long> = map { it.checkedId }
+  fun <E : WithEntity<E>> Iterable<Id<E>>.checkedIdsToArray(): Array<Long> = map { it.checkedId }.toLongArray().toTypedArray()
   fun <E : WithEntity<E>> Iterable<Id<E>>.checkedRefs(): Iterable<ConsumerRef> = map { it.checkedRef }
 }
